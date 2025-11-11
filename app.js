f93ec5cae88d5745185aea276a4e3b7f8eb39aea
minhtri1612 <10422119@student.vgu.edu.vn>
2025-11-11 14:17:05 +0700

    Async Patterns - Blocking Code

--- app.js
+++ app.js
@@ -1,8 +1,27 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    console.log('request event');
-    res.end('Hello World');
+
+    if (req.url === '/') {
+        res.end('Welcome to our home page');
+        return;
+    }
+    if (req.url === '/about') {
+        for (let i = 0; i < 1000; i++) {
+            for (let j = 0; j < 1000; j++) {
+                console.log(`${i} ${j}`);
+            }
+        }
+        res.end('Here is our short history');
+        return;
+    }
+
+    // 404 - fallback
+    res.end(`
+    <h1>Oops!</h1>
+    <p>We can't seem to find the page you are looking for</p>
+    <a href="/">back home</a>
+    `);
 })
 
 server.listen(5000, () => {
