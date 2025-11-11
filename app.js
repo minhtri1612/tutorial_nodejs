7614b34df57f640f05fc471782d0c886ea4ea2f4
minhtri1612 <10422119@student.vgu.edu.vn>
2025-11-11 12:12:05 +0700

    alternative module export

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 // Modules
 const names = require('./4-names')
 const sayHi = require('./5-utils')
-
+const data = require('./6-alternative')
+console.log(data)
 sayHi('minhtri')
 sayHi(names.john)
 sayHi(names.peter)
