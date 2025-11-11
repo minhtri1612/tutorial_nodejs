94e52bf0849162a205c373a2f5cc9d2f49d82264
minhtri1612 <10422119@student.vgu.edu.vn>
2025-11-11 11:33:00 +0700

    globals

---
+++ 2-globals.js
@@ -0,0 +1,16 @@
+// GLOBALS - NO WINDOW !
+
+// __dirname - path to current directory
+console.log(__dirname);
+
+// __filename - file name
+console.log(__filename);
+
+// require - function to use modules (CommonJS)
+console.log(require);
+
+// module - info about current module (file)
+console.log(module);
+
+// process - info about env where the program is being executed
+console.log(process);
