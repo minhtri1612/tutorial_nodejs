19162de5fa5cfefc9f3491c473418df5f5335b60
minhtri1612 <10422119@student.vgu.edu.vn>
2025-11-11 12:14:28 +0700

    sum in node

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 const names = require('./4-names')
 const sayHi = require('./5-utils')
 const data = require('./6-alternative')
+const addValues = require('./7-mind-grenade')
 console.log(data)
 sayHi('minhtri')
 sayHi(names.john)
