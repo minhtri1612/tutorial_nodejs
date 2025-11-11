7db8b81ea905fc13f19d3fd11fd7b96dbab65ad5
minhtri1612 <10422119@student.vgu.edu.vn>
2025-11-11 12:22:42 +0700

    os - module

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
-// Modules
-const names = require('./4-names')
-const sayHi = require('./5-utils')
-const data = require('./6-alternative')
-const addValues = require('./7-mind-grenade')
-console.log(data)
-sayHi('minhtri')
-sayHi(names.john)
-sayHi(names.peter)
+const os = require('os');
 
+// info about current user
+const user = os.userInfo();
+console.log(user);
+
+// method returns the system uptime in seconds
+console.log(`The system uptime is ${os.uptime()} seconds`);
+
+// current OS info
+const currentOS = {
+    name: os.type(),
+    release: os.release(),
+    totalMem: os.totalmem(),
+    freeMem: os.freemem(),
+}
+
+console.log(currentOS);
