e58086c3852aefd264dd07467d3de7837e39f8b2
minhtri1612 <10422119@student.vgu.edu.vn>
2025-11-11 13:00:40 +0700

    FS - module (async)

--- app.js
+++ app.js
@@ -1,14 +1,29 @@
-const { readFileSync, writeFileSync } = require('fs');
+const { readFile, writeFile } = require('fs');
 
-const first = readFileSync('./content/first.txt', 'utf8');
-const second = readFileSync('./content/second.txt', 'utf8');
+readFile('./content/first.txt', 'utf8', (err, result) => {
+    if (err) {
+        console.log(err);
+        return;
+    }
+    const first = result;
+    readFile('./content/second.txt', 'utf8', (err, result) => {
+        if (err) {
+            console.log(err);
+            return;
+        }
+        const second = result;
+        writeFile(
+            './content/result-sync.txt', 
+            `Here is the result: ${first}, ${second}`, 
+            (err, result) => {
+                if (err) {
+                    console.log(err);
+                    return;
+                }
+                console.log('done with this task');
+            }
+        );
+    });
+});
 
-console.log(first, second);
-
-writeFileSync(
-    './content/result-sync.txt', 
-    `Here is the result: ${first}, ${second}`, { flag: 'a' }
-);
-
-console.log('done with this task');
-console.log('starting the next one');
+console.log('starting next task');
