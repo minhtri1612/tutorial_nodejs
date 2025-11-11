a74accff1ff7ee19fd0d25d10dd36ffe784823c2
minhtri1612 <10422119@student.vgu.edu.vn>
2025-11-11 13:08:21 +0700

    Sync and Async

--- 10-fs-sync.js
+++ 10-fs-sync.js
@@ -1,5 +1,5 @@
 const { readFileSync, writeFileSync } = require('fs');
-
+console.log('start');
 const first = readFileSync('./content/first.txt', 'utf8');
 const second = readFileSync('./content/second.txt', 'utf8');
 
@@ -7,7 +7,8 @@ console.log(first, second);
 
 writeFileSync(
     './content/result-sync.txt', 
-    `Here is the result: ${first}, ${second}`, { flag: 'a' }
+    `Here is the result: ${first}, ${second}`, 
+    { flag: 'a' }
 );
 
 console.log('done with this task');
